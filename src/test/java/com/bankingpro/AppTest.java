356122dc07d5d65754d8cafbbfef5898b4fd7f30
bachi007 <chitturi.bhaskar007@gmail.com>
2023-04-26 19:43:36 +0530

    testing done

--- src/test/java/com/bankingpro/AppTest.java
+++ src/test/java/com/bankingpro/AppTest.java
@@ -8,49 +8,120 @@ public class AppTest{
 	
 	bankingdao b1=new bankingdao();
 	
-//	
+
 //	@Test
-//	//positive test case
+// // positive test case
 //	public void testlogin() throws Exception {
 //		
 //		b1.dbconnection();
 //		int exp=124;
 //		int res=b1.login("surya", 2505);//res is actual value from login	
 //		assertEquals(exp,res);
-//		
-//	}
+//}
 //	
 //	@Test
-//	//negative test case
+////  negative test case
 //	public void testlogin2() throws Exception {
 //		
 //		b1.dbconnection();
-//		int exp=124;
+//		int exp=0;
 //		int res=b1.login("sury", 2505);//res is actual value from login	
 //		assertEquals(exp,res);
 //		
 //	}
 //	
 //	@Test
-//	//negative with wrong password
+////  negative with wrong password
 //	public void testlogin3() throws Exception {
 //		
 //		b1.dbconnection();
-//		int exp=124;
+//		int exp=-1;
 //		int res=b1.login("surya", 2501);//res is actual value from login	
-//		assertEquals(exp,res);
+//	assertEquals(exp,res);
 //		
 //	}
+//	
+////	@Test
+////	public void testdeposit()throws Exception{
+////		
+////		b1.dbconnection();
+////		
+////		int exp=49000;
+////		int availamount=b1.deposit(3000, 101);
+////		assertEquals(exp,availamount);
+////	}
+////	
+//	
+////	@Test 
+////	public void testwithdraw()throws Exception {
+////		
+////		b1.dbconnection();
+////		
+////		int exp=31000;
+////		
+////		int amt=b1.withdraw(4000, 2505, 101);
+////		
+////		assertEquals(exp,amt);
+////	}
+//	
+//	@Test 
+//	public void testwithdraw()throws Exception {
+//		
+//		b1.dbconnection();
+//		
+//		int exp=-1;
+//		
+//		int amt=b1.withdraw(40000, 2505, 101);
+//		
+//		assertEquals(exp,amt);
+//	}
+//	
+//	
+//	@Test
+//	public void testpwd() throws Exception {
+//		
+//		int exp=1;
+//		
+//		b1.dbconnection();
+//		int res=b1.changepwd(2505, 1213, 101);
+//		assertEquals(exp,res);
+//	}
+//	
+//	@Test
+//	public void testdel() throws Exception {
+//		b1.dbconnection();
+//		
+//		int exp=1;
+//		int act=b1.deleteAccount(1213, 101);
+//		assertEquals(exp,act);
+//	}
+	
 	
 	@Test
-	public void testdeposit()throws Exception{
+	public void testregister() throws Exception {
 		
+		
+		customer c1=new customer();
+		c1.cusid=101;
+		c1.cusname="Bhaskara";
+		c1.cuspin=2505;
+		c1.cusamount=50000;
+		
+		
+		int exp=1;
 		b1.dbconnection();
 		
-		int exp=49000;
-		int availamount=b1.deposit(3000, 101);
-		assertEquals(exp,availamount);
+		int act=b1.registercustomer(c1);
+		assertEquals(exp,act);
+		
 	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
